ff5f3b13e745e0277b93c9705938a09a9552837e
Uday Raj <udayraj.vadeghar@gmail.com>
2024-01-24 02:15:06 +0530

    Plain Text decryption.
    The decryption of the plain text would not work in the previous code because transposition decryption , so henceforth n was replaced by n1 for such change.I couldnt really figure our on the encryption side , i will look through it in sometime .

--- src/main/java/com/thealgorithms/ciphers/ProductCipher.java
+++ src/main/java/com/thealgorithms/ciphers/ProductCipher.java
@@ -11,12 +11,14 @@ class ProductCipher {
         System.out.println(" ");
         System.out.println("Enter a number: ");
         int n = sc.nextInt();
+        int n1 = n;
+
 
         // Substitution encryption
         StringBuffer substitutionOutput = new StringBuffer();
         for (int i = 0; i < substitutionInput.length(); i++) {
             char c = substitutionInput.charAt(i);
-            substitutionOutput.append((char) (c + 5));
+            substitutionOutput.append((char) (c + n));
         }
         System.out.println(" ");
         System.out.println("Substituted text: ");
@@ -48,7 +50,7 @@ class ProductCipher {
         System.out.println(transpositionOutput);
 
         // Transposition decryption
-        n = transpositionOutput.length() / n;
+        n1 = transpositionOutput.length() / n1;
         StringBuffer transpositionPlaintext = new StringBuffer();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < transpositionOutput.length() / n; j++) {
@@ -61,7 +63,7 @@ class ProductCipher {
         StringBuffer plaintext = new StringBuffer();
         for (int i = 0; i < transpositionPlaintext.length(); i++) {
             char c = transpositionPlaintext.charAt(i);
-            plaintext.append((char) (c - 5));
+            plaintext.append((char) (c - n));
         }
 
         System.out.println("Plaintext: ");
